6a2889be05fc8d9c75b0a58c622d94f8bd2c564b
Corner <446042055@qq.com>
2021-02-08 14:37:17 +0800

    sendcloud settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
+u.email = "15996655506@163.com"           # 可以改成自己的 email
 
 u.password = "123456"                # 最少要六码
 
